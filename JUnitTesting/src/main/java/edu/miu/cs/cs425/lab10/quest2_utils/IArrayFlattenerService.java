cb62ffc991893bdecbf13f6a4b31dd2d49db4169
Filmon Tesfalem <fmisghna@miu.edu>
2021-07-10 01:11:12 -0500

    Second

---
+++ JUnitTesting/src/main/java/edu/miu/cs/cs425/lab10/quest2_utils/IArrayFlattenerService.java
@@ -0,0 +1,7 @@
+package edu.miu.cs.cs425.lab10.quest2_utils;
+
+public interface IArrayFlattenerService {
+
+    int[] flattenArray(int[][] input);
+
+}
---
+++ JUnitTesting/src/test/java/edu/miu/cs/cs425/lab10/quest2_utils/ArrayReverserTestSuit.java
@@ -0,0 +1,9 @@
+package edu.miu.cs.cs425.lab10.quest2_utils;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ArrayReverserTest.class,ArrayReverserNullTest.class})
+public class ArrayReverserTestSuit {
+}
